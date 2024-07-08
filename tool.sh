547e47614fcb9a2ff5d62f8734a2438de9b59b09
hacker-515m <bahaa0106400@gmail.com>
2024-07-08 22:10:17 +0300

    Add files via upload

---
+++ tool.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+clear
+echo -e "\033[31m 
+░░░░░░░░ ░░░░░░   ░░░░░░  ░░░    ░░░ ░░░░░░  ░░░░░░░ ░░░░░░  
+   ▒▒    ▒▒   ▒▒ ▒▒    ▒▒ ▒▒▒▒  ▒▒▒▒ ▒▒   ▒▒ ▒▒      ▒▒   ▒▒ 
+   ▒▒    ▒▒▒▒▒▒  ▒▒    ▒▒ ▒▒ ▒▒▒▒ ▒▒ ▒▒▒▒▒▒  ▒▒▒▒▒   ▒▒▒▒▒▒  
+   ▓▓    ▓▓   ▓▓ ▓▓    ▓▓ ▓▓  ▓▓  ▓▓ ▓▓   ▓▓ ▓▓      ▓▓   ▓▓ 
+   ██    ██████   ██████  ██      ██ ██████  ███████ ██   ██ 
+ \033[0m"
+ echo "Cerated Py Bahaa Al Maghribii"
+ echo "_____________________________"
+ echo "my phone number +201064004818 "
+ echo "______________________________"
+echo "My city Sohag , Egption "
+echo "*******************************"
+echo "*******************************"
+sleep 10
+
+pip3 install tbomb
+sudo apt install git
+git clone https://github.com/TheSpeedX/TBomb.git
+cd TBomb
+bash TBomb.sh
+
